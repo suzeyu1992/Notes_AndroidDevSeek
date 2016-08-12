96c65fa2950ea9d0fca45ce294f11846c5a3c149
suzeyu <suzeyu_1992@163.com>
2016-08-12 23:51:23 +0800

    init 7

---
+++ AndroidDevSeek_07/app/src/main/java/com/szysky/note/androiddevseek_07/MainActivity.java
@@ -0,0 +1,13 @@
+package com.szysky.note.androiddevseek_07;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
