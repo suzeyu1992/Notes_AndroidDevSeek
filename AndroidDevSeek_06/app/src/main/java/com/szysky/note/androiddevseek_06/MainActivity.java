99ab3972d998546cca32f3d3c530fd2bfe8643c2
suzeyu <suzeyu_1992@163.com>
2016-08-12 18:01:57 +0800

    练习第六章的系统常用的Drawable衍生的各种Drawable子类的xml布局创建

--- AndroidDevSeek_06/app/src/main/java/com/szysky/note/androiddevseek_06/MainActivity.java
+++ AndroidDevSeek_06/app/src/main/java/com/szysky/note/androiddevseek_06/MainActivity.java
@@ -1,13 +1,27 @@
 package com.szysky.note.androiddevseek_06;
 
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
